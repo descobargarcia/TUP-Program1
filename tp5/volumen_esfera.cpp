ad8230f01f6bc92ffedc0b5e8b04c061e02df7c0
David Escobar García <64324818+descobargarcia@users.noreply.github.com>
2022-04-16 23:58:33 -0300

    TP5 Prob 2 Volumen Esferico

---
+++ tp5/volumen_esfera.cpp
@@ -0,0 +1,29 @@
+/*Problema 2. Escribir un programa para calcular el volumen de 
+una esfera de radio "r" ingresado por consola*/
+
+#include <iostream>
+using namespace std;
+
+int main ()
+
+{
+    double PI = 3.141592653589793;
+    double r, volumen;
+    cout << endl;
+    cout << "Ingrese el radio de la esfera: " << endl;
+    cin >> r;
+    if (r<0)
+    {
+        cout << "ERROR: radio negativo" << endl;
+    }
+    else
+    {
+        volumen = (4.0 / 3.0)*PI*r*r*r;
+        cout << endl;
+        cout.precision(2);
+        cout << fixed;
+        cout << "Volumen de la esfera: " << volumen << endl;
+    }
+    cout << endl;
+    return 0;
+}
