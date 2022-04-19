d7964a207f46ab55d0ac6838f9af61973624570a
David Escobar García <64324818+descobargarcia@users.noreply.github.com>
2022-04-19 00:31:26 -0300

    TP8 mejor visualizacion del dado

--- tp8/dado_aleatorio.cpp
+++ tp8/dado_aleatorio.cpp
@@ -12,20 +12,41 @@ un número aleatorio y observando la salida correcta.
 *************************************************************************/
 
 #include <iostream>
+#include <string>
 #include <ctime>
+
 using namespace std;
 
 int main()
 {
-    int dado;
+    int numDado;
+    bool dadoEsPar;
+    string linSup, linMed, linInf;
     srand(time(0));
-    /*al dividir por 6, el modulo será un entero entre 0 y 5*/
-    dado = 1 + rand() % 6; 
+    //Al dividir por 6, el modulo será un entero entre 0 y 5
+    numDado = 1 + rand() % 6; 
+    dadoEsPar = (numDado%2 == 0);  
     cout << " _____________ " << endl;
     cout << "|             |" << endl;
-    cout << "|             |" << endl;
-    cout << "|      " << dado << "      |" << endl;
-    cout << "|             |" << endl;
+    //Definiendo linea superior e inferior
+    //numDado: 4, 5 o 6
+    if (numDado > 3) linSup = linInf = "|   *     *   |";
+    //numDado: 1
+    else if (numDado == 1) linSup = linInf = "|             |";
+    //numDado: 2 o 3
+    else
+    {
+        linSup = "|          *  |";
+        linInf = "|  *          |";
+    }
+    //Impresion de linea superior
+    cout << linSup << endl;
+    //Definiendo linea media
+    if (numDado == 6) linMed = linSup;
+    else linMed = dadoEsPar ? "|             |" : "|      *      |";
+    //Impresion de linea media e inferio
+    cout << linMed << endl;
+    cout << linInf << endl;
     cout << "|_____________|" << endl;
     return 0;
 }
